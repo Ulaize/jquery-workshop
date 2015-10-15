243084db37dff30057a49df23591f4700e0d5c27
Ulaize <ulaize@creative-tweed.com>
2015-10-15 21:37:48 +0000

    Exercise 4 without css

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // Write your code here!
 
+jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=montreal').then(
+    function(res) {
+        var lat = res.results[0].geometry.location.lat;
+        var lng = res.results[0].geometry.location.lng;
+        $('h2').after('<p>Find ISS here: ' + lat + ' ' + lng +'</p>');
 
-$('img').on('click', function(){
-    var $image = $('img').attr('src');
-    var $imageDescription = $('img').attr('alt');
-    console.log($imageDescription);
-    $('body').append('<div class="lightbox"></div>');
-    $('.lightbox').append('<img src=' + $image + '></img>');
-    $('.lightbox').append('<p>' + $imageDescription + '</p>');
-});
+    }
+);
