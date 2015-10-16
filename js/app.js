a2ea439f60dc8ef962c812499742ccf0228ca658
Ulaize <ulaize@creative-tweed.com>
2015-10-16 00:12:28 +0000

    challenge ISS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-// Write your code here!
+var $img = $('#iss-position');
 
-
-$('img').on('click', function(){
-    var $image = $('img').attr('src');
-    var $imageDescription = $('img').attr('alt');
-    console.log($imageDescription);
-    $('body').append('<div class="lightbox"></div>');
-    $('.lightbox').append('<img src=' + $image + '></img>');
-    $('.lightbox').append('<p>' + $imageDescription + '</p>');
-});
+jQuery.getJSON('http://api.open-notify.org/iss-now.json?callback=?').then(
+    function(res) {
+        console.log(res);
+        var lat = res.iss_position.latitude;
+        var lng = res.iss_position.longitude;
+        console.log(lat+' '+lng);
+        var googleMapsUrl = ("https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=13&size=500x400&maptype=roadmap&markers=color:green%7Clabel:G%7C40.711614,-74.012318&key=AIzaSyCFYgUuLsL7hhJEXH2FKSkbqlXb5DRUfmc");
+        $img.attr('src', googleMapsUrl);
+    }
+);
